46275df92bc910e7eebc06c28f1a834dd1ca4afe
Muhammad Awais <mawaismalik520@gmail.com>
2023-06-02 11:57:34 +0500

    Update Layout.js

--- src/com/Layout.js
+++ src/com/Layout.js
@@ -36,7 +36,7 @@ const Navbar = () => {
             onClick={() => {
               setDir(!dir);
               document.documentElement.setAttribute("dir", "rtl");
-              console.log("Hello Main");
+              console.log("Hello Sub");
             }}
           >
             Left
